a434a014fba2390426dbf1f994a361d4fa379870
Jakub Ciura <jakub.ciura@gmail.com>
2025-12-14 20:52:33 +0100

    update - calculating e_ir matrix (data frame) - ammount of visits in system i, class r

--- params.py
+++ params.py
@@ -51,8 +51,7 @@ NODE_NAMES = {
     8: "Stała eksploatacja"
 }
 
-# mi
-# Domyślne średnie szybkości obsługi (mi = 1/mean_service_time).
+# mu - szybkości obsługi węzłów (1 / średni czas obsługi [min])
 # Dla rozkładu wykładniczego: time ~ Exp(rate)
 SERVICE_RATES = {
     1: 1/3.0,   # node 1: średnio 3.0 czasu na obsługę
@@ -64,7 +63,6 @@ SERVICE_RATES = {
     7: 1/2.0,   # node 7: obsługa klienta średnio 2.0
     8: 1/10.0   # node 8: stała eksploatacja średnio 10.0
 }
-
 
 # Trasy klas (lista węzłów do odwiedzenia; po dojściu do ostatniego węzła wraca do 1).
 # ROUTING[class][node] = [(next_node, probability), ...]
@@ -111,3 +109,46 @@ SIM_TIME = 20000.0
 
 # Ziarno generatora losowego (dla powtarzalności)
 SEED = 42
+
+
+import numpy as np
+import pandas as pd
+
+
+
+def compute_e_ir(ROUTES, I, classes):
+    """
+    Oblicza liczby wizyt e_ir dla każdej klasy r i stacji i.
+    ROUTES: słownik tras
+    I: liczba stacji
+    classes: lista klas np. [1,2,3,4]
+    """
+    e_ir = {i: {r: 0.0 for r in classes} for i in range(1, I+1)}
+
+    for r in classes:
+        # budujemy macierz P^(r)
+        P = np.zeros((I, I))
+        routes_r = ROUTES.get(r, {})
+        for j in range(1, I+1):
+            if j in routes_r:
+                for nxt, p in routes_r[j]:
+                    P[j-1, nxt-1] += p
+
+        # układ: e^T = e^T P, czyli (P^T - I)^T e = 0
+        A = (P.T - np.eye(I))
+        # zastępujemy ostatni wiersz warunkiem normalizacji
+        A[-1, :] = np.ones(I)
+        b = np.zeros(I)
+        b[-1] = 1.0
+
+        e = np.linalg.lstsq(A, b, rcond=None)[0]
+
+        for i in range(I):
+            e_ir[i+1][r] = float(e[i])
+
+    return e_ir
+
+
+e_ir=compute_e_ir(ROUTES, len(NODE_TYPES), list(POPULATION.keys()))
+df = pd.DataFrame(e_ir).T
+print(df.round(3))
